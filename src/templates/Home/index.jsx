dbc7715816cea8f963b5d2d5a86b7cad9f348364
rafael-leivas <rafaelblsteam@gmail.com>
2024-02-25 16:26:46 -0300

    Implementing a card search system

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -2,12 +2,18 @@ import { Component } from "react";
 
 import "./styles.css";
 
-import { Posts } from '../../components/Posts';
+import { Posts } from "../../components/Posts";
 import { loadPosts } from "../../utils/load-posts";
+import { Button } from "../../components/Button";
+import { TextInput } from "../../components/TextInput";
 
 export class Home extends Component {
   state = {
     posts: [],
+    allPosts: [],
+    page: 0,
+    postsPerPage: 5,
+    searchValue: "",
   };
 
   async componentDidMount() {
@@ -15,18 +21,63 @@ export class Home extends Component {
   }
 
   loadPosts = async () => {
+    const { page, postsPerPage } = this.state;
+
     const postsAndPhotos = await loadPosts();
-    this.setState({ posts: postsAndPhotos });
+    this.setState({
+      posts: postsAndPhotos.slice(page, postsPerPage),
+      allPosts: postsAndPhotos,
+    });
+  };
+
+  loadMorePosts = () => {
+    const { page, postsPerPage, allPosts, posts } = this.state;
+    const nextPage = page + postsPerPage;
+    const nextPosts = allPosts.slice(nextPage, nextPage + postsPerPage);
+    posts.push(...nextPosts);
+
+    this.setState({ posts, page: nextPage });
+  };
+
+  handleChange = (e) => {
+    const { value } = e.target;
+    this.setState({ searchValue: value });
   };
 
   render() {
-    const { posts } = this.state;
+    const { posts, page, postsPerPage, allPosts, searchValue } = this.state;
+    const noMorePosts = page + postsPerPage >= allPosts.length;
+
+    const filteredPosts = !!searchValue
+      ? allPosts.filter((post) => {
+          return post.title.toLowerCase().includes(searchValue.toLowerCase());
+        })
+      : posts;
 
     return (
       <section className="container">
-        <Posts posts={posts} />
+        <div className="search-container">
+          {!!searchValue && <h1>Search Value: {searchValue}</h1>}
+
+          <TextInput
+            searchValue={searchValue}
+            handleChange={this.handleChange}
+          />
+        </div>
+
+        {filteredPosts.length > 0 && <Posts posts={filteredPosts} />}
+        {filteredPosts.length === 0 && <p>Não existem posts =(</p>}
+        <div className="button-container">
+          {!searchValue && (
+            <Button
+              text="LoadMorePosts"
+              onClick={this.loadMorePosts}
+              disabled={noMorePosts}
+            />
+          )}
+        </div>
       </section>
-    )
+    );
   }
 }
 export default Home;
